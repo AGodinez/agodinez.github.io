b79eff422eed237ba641289d5b9f310c5e69fd99
AGodinez <pistilloagodinez@gmail.com>
2014-10-12 21:55:58 -0600

    Agregue efecto JParallax

--- public/js/main.js
+++ public/js/main.js
@@ -1,38 +1,12 @@
-  var imagenesSlider = new Array(".item1",".item2",".item3");
-  var count = 0;
-
-  var transicion = function(){
-    ocultar(imagenesSlider[count]);
-    count = (count + 1) % 3;
-    mostrar(imagenesSlider[count]);
-  }
-  
-  function mostrar(img){
-    $(img).ready(function(){        
-        $(imagenesSlider[count]).fadeIn(1500);    
-    });
-  }
-  
-  function ocultar(img){
-    $(img).ready(function(){        
-        $(imagenesSlider[count]).fadeOut(1500);   
-    });
-  }
-
-  $('label.navegacion-prev').click(function prevImg(){
-    ocultar(imagenesSlider[count]);
-    count = (count + 2) % 3;
-    mostrar(imagenesSlider[count]);
-  });
-
-  $('label.navegacion-next').click(function nextImg(){
-    ocultar(imagenesSlider[count]);
-    count = (count + 1) % 3;
-    mostrar(imagenesSlider[count]);
-  });
-
-$(document)
-  .ready(function() {
+$(document).ready(function(){
+  //JParallax
+  var target = jQuery("#target");
+  target.children('img').parallax(
+    { mouseport: target },
+    { xparallax: 0.2, yparallax: 0.2 },     // Imagen 1
+    { xparallax: 0.6, yparallax: 0.6 },     // Imagen 2
+    {}                                      // Imagen 3
+  );
 
     //Moverse por toda la pagina
   $('a[href*=#]').click(function() {
@@ -48,14 +22,12 @@ $(document)
     }
   });
   
-  //Iniciar elmenu desplegable
+  //Menu colapsable
   $('.ui.dropdown')
-      .dropdown({
-        on: 'hover'
-      })
-  ;
+    .dropdown({
+      on: 'hover'
+  }); 
+});
 
-  //Slider
-  setInterval(transicion, 6000);
 
-  });
+  
